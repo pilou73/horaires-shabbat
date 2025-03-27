1fa6a2cc0182b622735db43c5e0ff98d7827a2db
pilou73 <philippe73@gmail.com>
2025-03-27 17:17:09 +0200

    SUMMER

--- horaires_shabbat.py
+++ horaires_shabbat.py
@@ -134,11 +134,11 @@ class ShabbatScheduleGenerator:
             'parashat_hashavua': self.round_to_nearest_five(end_minutes - (3 * 60)),
             'tehilim': self.round_to_nearest_five(17 * 60),
             'nashim': self.round_to_nearest_five(16 * 60),
-            'shiur_rav': self.round_to_nearest_five(end_minutes - (2 * 60 + 15))
+            'shiur_rav': self.round_to_nearest_five(end_minutes - (2 * 60 + 25))
         }
         
         times['mincha_2'] = self.round_to_nearest_five(times['shiur_rav'] + 45)
-        times['arvit'] = self.round_to_nearest_five(end_minutes - 5)
+        times['arvit'] = self.round_to_nearest_five(end_minutes - 10)
         
         return times
 
